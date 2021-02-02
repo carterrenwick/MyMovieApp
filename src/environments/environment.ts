992f6932481caa41fc485611f7917cda06671cf7
Carter Taylor <cartertaylor11@yahoo.com>
2021-02-01 22:57:30 -0600

    changed local tomcat port to 8080

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const baseApiUrl = 'http://localhost:8989/MyMovieApi/api';
+const baseApiUrl = 'http://localhost:8080/MyMovieApi/api';
 export const environment = {
   production: false,
 
